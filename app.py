2d5be2cbf35ca9902f247af717111365e22b57c4
Deike <deikerv@gmail.com>
2020-05-09 18:05:54 -0400

    Ejercicio culminado

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import csv
 todos = []
 stop = False
 
@@ -7,23 +8,58 @@ def get_todos():
 
 def add_one_task(title):
     # your code here
+    todos.append(title)
+    return todos
     pass
 
 def print_list():
+    contador=1
     global todos
+    for x in todos:
+        print(str(contador)+': '+x)
+        contador=contador+1
     pass
 
 def delete_task(number_to_delete):
     # your code here
+    number_to_delete=int(number_to_delete)
+    number_to_delete=number_to_delete-1
+    todos.pop(number_to_delete)
+    return todos
     pass
 
 def save_todos():
-    # your code here
+    file_tareas=""
+    i=0
+    for x in todos:
+        if i==0:
+            #print(x['name'])
+            file_tareas=file_tareas +x
+            i=i+1
+        else:
+            file_tareas=file_tareas+','+x
+    print(file_tareas)
+
+    file_to_save=open("list_tareas.csv","w+")
+    file_to_save.write(file_tareas)
+    file_to_save.close()
     pass
 
     
 def load_todos():
     # your code here
+    file=open("list_tareas.csv","r")
+    csv_file=csv.reader(file)
+    for row in csv_file:
+        #print(row)
+        lista=row
+        for x in lista:
+            #print(x)
+            if x not in todos:
+                todos.append(x)
+    #print(todos)
+    file.close()   
+    return todos
     pass
 
 # Below this code will only run if the entry file running was app.py
@@ -43,6 +79,7 @@ if __name__ == '__main__':
             stop = True
         elif response == "3":
             print_list()
+            
         elif response == "2":
             print("What task number you want to delete?")
             number_to_delete = input()
